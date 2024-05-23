378e2886989f7a96eade7725395fe6402b11704b
Laura Fuentes <68988856+laurafv@users.noreply.github.com>
2024-05-23 15:18:31 +0000

    laurafuentes

---
+++ C01/ex07 [HELP NEEDED]/ft_rev_int_tab.c
@@ -0,0 +1,11 @@
+/* 
+Create a function which reverses a given array of integer (first goes last, etc). 
+The arguments are a pointer to int and the number of ints in the array.
+*/
+
+#include<unistd.h>
+
+void    ft_rev_int_tab (int *tab, int   size)
+{
+    
+}
